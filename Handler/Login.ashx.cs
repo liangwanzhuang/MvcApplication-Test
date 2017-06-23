50c2d1f67d73eff20319c8959ee270df7b6d7d0d
liangwanzhuang <184533033@qq.com>
2017-06-23 13:54:58 +0800

    Initial commit
    fsdf

---
+++ Handler/Login.ashx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MvcApplication_Test
+{
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string action = context.Request.Params["key"]; 
+            string status = "2";
+            string error = "1";
+            StringBuilder json = new StringBuilder();
+            json.Append("{");
+            json.Append("\"status\":" + "\"" + status + "\",");
+            json.Append("\"error\":" + "\"" + error + "\"");
+            json.Append("}");
+
+            string returnStr = json.ToString();
+            context.Response.Write(returnStr);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
